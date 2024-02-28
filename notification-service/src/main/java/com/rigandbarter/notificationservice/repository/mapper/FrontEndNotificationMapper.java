f51044c54bc9107617ff933f18b3bae4f0fa6800
Connor Shore <cjshore12@gmail.com>
2024-02-27 17:13:01 -0700

    RAB-35: Added mappers for all entity/dtos

--- notification-service/src/main/java/com/rigandbarter/notificationservice/repository/mapper/FrontEndNotificationMapper.java
+++ notification-service/src/main/java/com/rigandbarter/notificationservice/repository/mapper/FrontEndNotificationMapper.java
@@ -7,6 +7,13 @@ import java.util.UUID;
 
 public class FrontEndNotificationMapper {
 
+    /**
+     * Converts the event to a Front End Notification
+     * @param event The event to convert
+     * @param title The title (header) of the frontend notification
+     * @param body The contents of the front end notification
+     * @return The created frontend notification
+     */
     public static FrontEndNotification fromEvent(RBEvent event, String title, String body) {
         return FrontEndNotification.builder()
                 .id(UUID.randomUUID().toString())
--- transaction-service/src/main/java/com/rigandbarter/transactionservice/repository/mapper/TransactionMapper.java
+++ transaction-service/src/main/java/com/rigandbarter/transactionservice/repository/mapper/TransactionMapper.java
@@ -8,7 +8,13 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class TransactionMapper {
-    public static Transaction fromRequestDto(TransactionRequest transactionRequest) {
+
+    /**
+     * Converts TransactionRequest to Transaction entity
+     * @param transactionRequest The transaction request to convert
+     * @return The converted Transaction entity
+     */
+    public static Transaction dtoToEntity(TransactionRequest transactionRequest) {
         return Transaction.builder()
                 .uniqueId(UUID.randomUUID().toString())
                 .buyerId(transactionRequest.getBuyerId())
